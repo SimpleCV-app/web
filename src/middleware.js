0ff54a7105b549dcaab86851dbc1e5dfb83cf028
alf1e <alfiewbarlow@gmail.com>
2025-02-08 19:18:36 +0000

    I should learn to remove logging

--- src/middleware.js
+++ src/middleware.js
@@ -12,7 +12,6 @@ export const onRequest = defineMiddleware(async ({ request, rewrite, url, locals
 
     const meta = await databases.getDocument(import.meta.env.DB0_ID, "meta", "ROOT")
     if (meta.maintenance_mode && url.pathname != "/e/maintenance_mode" && !url.pathname.startsWith("/admin")) {
-      console.log("maintenance mode")
       return rewrite("/e/maintenance_mode")
     }
   } catch {}
